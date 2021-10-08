3e29c52f0a70a7b110cb840e6fdfaf1360f32b4e
Timur Demin <me@tdem.in>
2021-10-08 18:36:24 +0500

    Check for negative indices in Slice

--- internal/containers/slice.go
+++ internal/containers/slice.go
@@ -10,7 +10,7 @@ func NewSlice(size int) Slice {
 // Set performs copy() and returns its result. It is up to the caller to
 // check whether the result is equal to len(data).
 func (s Slice) Set(offset int, data []byte) int {
-	if len(s) <= offset {
+	if len(s) <= offset || offset < 0 {
 		return 0
 	}
 	return copy(s[offset:], data)
@@ -20,6 +20,9 @@ func (s Slice) Set(offset int, data []byte) int {
 // larger than Slice length, it will return nil. If offset+size overlaps
 // the Slice, it will return Slice[offset:].
 func (s Slice) Get(offset, size int) []byte {
+	if offset < 0 || size < 0 {
+		return nil
+	}
 	if len(s) <= offset {
 		return nil
 	}
